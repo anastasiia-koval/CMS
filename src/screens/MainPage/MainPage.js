3fd152827b749b809f037e06e117e21905529cf4
vlad <vladiachuk@gmail.com>
2021-12-05 23:57:40 +0100

    2

--- src/screens/MainPage/MainPage.js
+++ src/screens/MainPage/MainPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Specialists from "../../components/specialists/Specialists";
 
 const MainPage = () => {
-  return <div>Hello this is main page</div>;
+  return <div>Hello this is main page 22</div>;
 };
 
 export default MainPage;
