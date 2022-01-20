ffe81d9c20af06dbfa999ac1b449382eae0d7446
Michał Pazur <s19023@pjwstk.edu.pl>
2022-01-20 02:36:47 +0100

    CMS-34 appointment list in profile

--- src/screens/UserAccountPage/UserAccountPage.jsx
+++ src/screens/UserAccountPage/UserAccountPage.jsx
@@ -1,7 +1,20 @@
-import { Typography, Box, makeStyles, Paper } from "@material-ui/core";
-import React, { useContext, useState } from "react";
-// import Reservations from "../../components/Reservations/Reservations";
+import {
+  Typography,
+  Box,
+  makeStyles,
+  Paper,
+  CircularProgress,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
+import moment from "moment";
+import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../../context/User/UserContext";
+import axiosInstance from "../../util/axiosInstance";
+const { REACT_APP_MY_ENV } = process.env;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     flexDirection: "row",
     width: "100%",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
   },
   userInfoCard: {
     display: "flex",
@@ -19,23 +35,60 @@ const useStyles = makeStyles((theme) => ({
     width: "50%",
     boxShadow: theme.shadows[3],
     borderRadius: theme.shape.borderRadius,
+    marginRight: "20px",
+    [theme.breakpoints.down("xs")]: {
+      marginRight: "0px",
+      marginBottom: "20px",
+      width: "100%",
+      maxWidth: "300px",
+    },
   },
   accountInfo: {
+    display: "flex",
+    flexDirection: "column",
     width: "100%",
     borderRadius: theme.shape.borderRadius,
     boxShadow: theme.shadows[3],
     minHeight: "300px",
     padding: "20px",
-    marginLeft: "20px",
   },
   text: {
     marginBottom: "15px",
   },
+  loader: {
+    width: "100%",
+    flexGrow: "1",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 }));
 const UserAccountPage = () => {
   const classes = useStyles();
-  const { username, email } = useContext(UserContext);
-  const [myReservations, setMyReservations] = useState();
+  const { username, userId, email, role } = useContext(UserContext);
+  const [reservations, setReservations] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const url =
+      role.name === "Specialist"
+        ? "/reservations?specialist.user="
+        : "/reservations?user.id=";
+    axiosInstance
+      .get(
+        `${REACT_APP_MY_ENV}${url}${userId}&date_gte=${moment().format(
+          "YYYY-MM-DD"
+        )}`
+      )
+      .then((response) => {
+        setReservations(response.data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className={classes.root}>
       <Box component={Paper} className={classes.userInfoCard}>
@@ -48,12 +101,40 @@ const UserAccountPage = () => {
       </Box>
       <Box component={Paper} className={classes.accountInfo}>
         <Typography className={classes.text} variant="h3">
-          Moje rezerwacje
+          Nadchodzące rezerwacje
         </Typography>
-        {/* TODO 
-          Display all user reservations
-        */}
-        {/* <Reservations /> */}
+        {isLoading ? (
+          <Box className={classes.loader}>
+            <CircularProgress size={48} />
+          </Box>
+        ) : (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Data rezerwacji</TableCell>
+                <TableCell>Godzina rezerwacji</TableCell>
+                <TableCell>
+                  {role.name === "Specialist" ? "Klient" : "Mechanik"}
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {reservations.map((reservation) => (
+                <TableRow>
+                  <TableCell>
+                    {moment(reservation.date).format("DD.MM.YYYY")}
+                  </TableCell>
+                  <TableCell>{reservation.time?.split(":")[0]}:00</TableCell>
+                  <TableCell>
+                    {role.name === "Specialist"
+                      ? reservation.user.username
+                      : `${reservation.specialist.name} ${reservation.specialist.surname}`}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </Box>
       <div></div>
     </div>
